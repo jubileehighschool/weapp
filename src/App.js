cafd4894f57c677ef7817487cf205e72058a61b1
saud ikram <saudikram62@gmail.com>
2023-04-29 18:35:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,30 @@ function App() {
     
     
     </div>
+    </Router>import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from "./components/Home"
+import Page1 from './components/Page1';
+function App() {
+  return (
+    <Router>    
+    <div className= 'App' >
+    
+    <Routes>
+      <Route path='/' element={<div className='home'> <Navbar/> <Home/></div>}/>
+      
+    </Routes>
+    
+    
+    </div>
     </Router>
+  );
+}
+
+export default App;
+
   );
 }
 
