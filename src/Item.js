9774ea28a5d828967af05a912027174ba5126597
saud ikram <saudikram62@gmail.com>
2023-04-29 18:34:37 +0530

    Add files via upload

---
+++ src/Item.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Item(props) {
+  return (
+    <div>
+        <img alt='hyderabad schools' src="https://images.unsplash.com/photo-1516321497487-e288fb19713f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2Nob29sJTIwZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"></img>
+    </div>
+  )
+}
+
+export default Item
