80b4bec3025418313699603b21a3083ea3d1e18d
saud ikram <saudikram62@gmail.com>
2023-04-29 18:39:35 +0530

    added page 7 & 8

---
+++ src/components/Page7.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./page7.css"
+function page7() {
+  return (
+    <div className='page7'>
+      <div div className='top'>
+        <div className='text'>
+        <h1>Let Us Show You Around</h1>
+        <h3>contact us to shedule a visit and experience our school</h3>
+        </div>
+        <div className='btn' >
+        <button className='button' >Contact Us</button>
+        </div>
+      </div>
+      
+      <div className='events'>
+       
+       {/* <EventSlider/> */}
+      </div>
+    </div>  
+    
+  )
+}
+
+export default page7
